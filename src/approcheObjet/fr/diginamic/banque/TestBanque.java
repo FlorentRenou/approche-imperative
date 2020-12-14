e6bf5237e5284622305bafe8db8031e5effc3d8f
FlorentRenou <florent.renou@epsi.fr>
2020-12-14 11:17:10 +0100

    TP 5 Le main retourne l'adresse de l'objet

---
+++ src/approcheObjet/fr/diginamic/banque/TestBanque.java
@@ -0,0 +1,15 @@
+package approcheObjet.fr.diginamic.banque;
+
+import approcheObjet.fr.diginamic.banque.entites.Compte;
+
+public class TestBanque {
+
+    public static void main(String[] args) {
+
+        Compte compte1 = new Compte(1,3000);
+
+        System.out.println(compte1);
+
+    }
+
+}
---
+++ src/approcheObjet/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,11 @@
+package approcheObjet.fr.diginamic.banque.entites;
+
+public class Compte {
+
+    private Integer numero,solde;
+
+    public Compte(Integer numero, Integer solde) {
+        this.numero = numero;
+        this.solde = solde;
+    }
+}
